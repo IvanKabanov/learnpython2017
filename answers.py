8d50915a9d387be4ab6ffc2c5cdede111e7d22ab
Ivan Kabanov <ivan.i.kabanov@gmail.com>
2017-03-11 17:40:54 +0300

    Addes answers.py

--- answers.py
+++ answers.py
@@ -1,9 +1,9 @@
 def get_answer(question):
-	answers = {'привет': 'И тебе привет!', 'как дела?': 'Отлично!', 'до свидания': 'пока'}
-	lower_question = question.lower()
-	print(answers.get(lower_question, 'Don\'t understand you!'))
-	return answers.get(lower_question)
-	
+    answers = {'привет': 'И тебе привет!', 'как дела?': 'Отлично!', 'до свидания': 'пока'}
+    lower_question = question.lower()
+    print(answers.get(lower_question, 'Don\'t understand you!'))
+    return answers.get(lower_question)
+    
 user_question = input('Enter question ')
 get_answer(user_question)
 
